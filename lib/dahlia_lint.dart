023f52962e9c74891eaeffc3ac8b07c824eacb38
Lars <larsb24@outlook.com>
2022-04-28 12:53:42 +0200

    Initial Code

---
+++ lib/dahlia_lint.dart
@@ -0,0 +1,14 @@
+/// The lint package doesn't ship any dart source code.
+///
+/// 1. Add it to your dev_dependencies
+/// ```yaml
+/// dev_dependencies:
+///   git: https://github.com/dahliaOS/dahlia_lint.git
+/// ```
+///
+/// 2. Include the rules into your `analysis_options.yaml`
+/// ```yaml
+/// include: package:dahlia_lint/analysis_options.yaml
+/// ```
+
+library dahlia_lint;
